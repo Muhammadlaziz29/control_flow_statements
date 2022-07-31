5573cc0fc63405cd22323405b4c782a0844b4d4e
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-07-31 23:01:43 +0500

    hello

--- bin/if09.dart
+++ bin/if09.dart
@@ -10,4 +10,21 @@
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False. */
 
-void main() {}
+String func(int a) {
+    String h = ' '; 
+    int x = a;
+    int x1 = x%10;
+    int x2 = x~/10;
+    int w = x1*10+x2;
+    if (a>w) {
+        h = 'True';
+    }
+    else{
+        h = 'False';
+    }
+return h;
+}
+
+void main() {
+    print(func(43));
+}
