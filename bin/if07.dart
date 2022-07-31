b9a7e4e854ed2c72512702ad5465521a74ab34d4
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-07-31 20:47:24 +0500

    hello

--- bin/if07.dart
+++ bin/if07.dart
@@ -11,4 +11,36 @@
         a: integer
     Returns:
         string: the message to print */
-void main() {}
+
+String func(int a) {
+
+    String w = ' ';
+    if (a>0) {
+        if (a%2==0) {
+            w = 'Positive even number';
+        }
+        if (a>0) {
+            if (a%2!=0) {
+                w = 'Positive odd number';
+            }
+        }
+        
+    }
+
+    if (a<0) {
+        if (a%2==0) {
+            w = 'Negative even number';
+        }
+        if (a<0) {
+            if(a%2!=0) {
+                w = 'Negative odd number';
+            }
+        }
+    }
+    return w;
+
+}
+
+void main() {
+    print(func(-311));
+}
